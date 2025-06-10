1617ebb720c2b372ba56d5bb3811ee79669efd70
Nikesh Urkude <110988738+nurkudeIDBS@users.noreply.github.com>
2025-06-10 14:55:51 +0100

    Added test when album is not in 100 then use default price. Refactoring of test cases.

--- src/test/java/com.cdwarehouse/TopAlbumsTest.java
+++ src/test/java/com.cdwarehouse/TopAlbumsTest.java
@@ -2,11 +2,24 @@ package com.cdwarehouse;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class TopAlbumsTest {
+
     @Test
-    void top100AlbumsPriceReduction() {
+    void top100Albums() {
+        Payment payment = mock(Payment.class);
+        NotifySales notifySale = mock(NotifySales.class);
+        TopAlbums topAlbums = mock(TopAlbums.class);
+        CD cd = new CD(payment, notifySale);
+        CompetitorPrice competitorPrice = mock(CompetitorPrice.class);
+        cd.getPrice("ABC", topAlbums, competitorPrice);
+        verify(topAlbums).checkAlbumIsInTop("ABC", 100);
+    }
+
+    @Test
+    void competitorPrice() {
         Payment payment = mock(Payment.class);
         NotifySales notifySale = mock(NotifySales.class);
         TopAlbums topAlbums = mock(TopAlbums.class);
@@ -14,8 +27,30 @@ public class TopAlbumsTest {
         CompetitorPrice competitorPrice = mock(CompetitorPrice.class);
         when(topAlbums.checkAlbumIsInTop("ABC", 100)).thenReturn(true);
         cd.getPrice("ABC", topAlbums, competitorPrice);
-        verify(topAlbums).checkAlbumIsInTop("ABC", 100);
-
         verify(competitorPrice).getCompetitorPrice("ABC");
+
+    }
+
+    @Test
+    void top100AlbumsPriceReduction(){
+        Payment payment = mock(Payment.class);
+        NotifySales notifySale = mock(NotifySales.class);
+        TopAlbums topAlbums = mock(TopAlbums.class);
+        CD cd = new CD(payment, notifySale);
+        CompetitorPrice competitorPrice = mock(CompetitorPrice.class);
+        when(topAlbums.checkAlbumIsInTop("ABC", 100)).thenReturn(true);
+        when(competitorPrice.getCompetitorPrice("ABC")).thenReturn(19.99);
+        assertEquals(18.99, cd.getPrice("ABC", topAlbums,competitorPrice));
+    }
+
+    @Test
+    void albumNotInTop100(){
+        Payment payment = mock(Payment.class);
+        NotifySales notifySale = mock(NotifySales.class);
+        TopAlbums topAlbums = mock(TopAlbums.class);
+        CD cd = new CD(payment, notifySale);
+        CompetitorPrice competitorPrice = mock(CompetitorPrice.class);
+        when(topAlbums.checkAlbumIsInTop("ABC", 100)).thenReturn(false);
+        assertEquals(9.99,cd.getPrice("ABC", topAlbums, competitorPrice));
     }
 }
